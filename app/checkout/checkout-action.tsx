460fea189db95a55835a81556aa283f72c3ecf27
DhineshT99 <dhinesht00@gmai.com>
2025-10-24 15:27:12 +0530

    final commit

--- app/checkout/checkout-action.tsx
+++ app/checkout/checkout-action.tsx
@@ -1,36 +1,33 @@
-// /app/checkout/checkout-action.ts
-
 "use server";
 
 import { CartItem } from "@/context/CartContext";
 import { stripe } from "@/lib/stripe";
 import { redirect } from "next/navigation";
 
-
 export const checkoutAction = async (formData: FormData): Promise<void> => {
-    const itemsJson = formData.get("items") as string;
-    const items = JSON.parse(itemsJson) as CartItem[];
-    
-    const totalInCents = items.reduce(
-        (acc, item) => acc + item.price * item.quantity,
-        0
-    );
+  const itemsJson = formData.get("items") as string;
+  const items = JSON.parse(itemsJson) as CartItem[];
 
-    const line_items = items.map((item: CartItem) => ({
-      price_data: {
-        currency: "cad",
-        product_data: { name: item.name },
-        unit_amount: item.price,
-      },
-      quantity: item.quantity,
-    }));
-       const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      line_items,
-      mode: "payment",
-      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
-      cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout`,
-    });
+  const totalInCents = items.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
 
-    redirect(session.url!);
+  const line_items = items.map((item: CartItem) => ({
+    price_data: {
+      currency: "cad",
+      product_data: { name: item.name },
+      unit_amount: item.price,
+    },
+    quantity: item.quantity,
+  }));
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items,
+    mode: "payment",
+    success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
+    cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout`,
+  });
+
+  redirect(session.url!);
 };
